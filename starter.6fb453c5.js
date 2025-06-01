372c46940b105fe87d399a67c08683713b97d2ff
SLFatemi <sinalotfifatemi@gmail.com>
2025-06-01 10:49:14 +0430

    Updates

---
+++ starter.6fb453c5.js
@@ -0,0 +1,2 @@
+let recipeContainer=document.querySelector(".recipe"),timeout=function(e){return new Promise(function(o,t){setTimeout(function(){t(Error(`Request took too long! Timeout after ${e} second`))},1e3*e)})},myArray=[];
+//# sourceMappingURL=starter.6fb453c5.js.map
