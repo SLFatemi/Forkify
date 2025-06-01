306c7e080278c73093d60a73d4c0c1ff6d04e8c1
SLFatemi <sinalotfifatemi@gmail.com>
2025-06-01 11:28:32 +0430

    Lecture #5

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,8 +1,8 @@
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
+const timeout = function(s) {
+  return new Promise(function(_, reject) {
+    setTimeout(function() {
       reject(new Error(`Request took too long! Timeout after ${s} second`));
     }, s * 1000);
   });
@@ -12,3 +12,33 @@ const timeout = function (s) {
 // https://forkify-api.jonas.io
 
 ///////////////////////////////////////
+
+
+async function showRecipe() {
+  try {
+    const res = await fetch('https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886');
+
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      serving: recipe.serving,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients
+
+    };
+    console.log(recipe);
+  } catch (e) {
+    alert(e);
+  }
+  return null;
+}
+
+(async () => {await showRecipe();})();
+
